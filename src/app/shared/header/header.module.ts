2ec36080127b581cf01b82ecdbbbab256c16ecb3
bhanuwhite <feather.white@gmail.com>
2020-10-14 13:32:15 +0530

    Adding dynamic card project

--- src/app/shared/header/header.module.ts
+++ src/app/shared/header/header.module.ts
@@ -4,12 +4,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { SideBarComponent } from './component/side-bar/side-bar.component';
 
 @NgModule({
   imports: [CommonModule, MatIconModule, MatSidenavModule, RouterModule],
-  declarations: [HeaderComponent, SideBarComponent],
+  declarations: [HeaderComponent],
   exports: [HeaderComponent],
-  entryComponents: [SideBarComponent],
 })
 export class HeaderModule {}
